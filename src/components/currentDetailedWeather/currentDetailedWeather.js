4d02c23d1f64e5c8f899fa96948d33d5ed6ac9e9
rita.radovics <rita.radovics@agillic.com>
2019-10-08 12:32:15 +0200

    working app, before cleanup

--- src/components/currentDetailedWeather/currentDetailedWeather.js
+++ src/components/currentDetailedWeather/currentDetailedWeather.js
@@ -1,11 +1,45 @@
 import React from 'react'
 import './currentDetailedWeather.css'
+import TempMax from '../icons/tempMaxIcon.js'
+import TempMin from '../icons/tempMinIcon.js'
+import SunRise from '../icons/sunRiseIcon.js'
+import SunSet from '../icons/sunSetIcon.js'
+import Wind from '../icons/windIcon'
 
-const CurrentDetailedWeather = ({description, humidity, tempMax, tempMin, sunRise, sunSet, wind}) => {
+const CurrentDetailedWeather = ({description, tempMax, tempMin, sunRise, sunSet, wind}) => {
+  const getDatestampInHourAndMinute = (time) => {
+    var date = new Date(time * 1000)
+    var hours = date.getHours()
+    var minutes = "0" + date.getMinutes()
+    return hours + ':' + minutes.substr(-2)
+    }
+
+  const descriptionCapitalized = description.charAt(0).toUpperCase() + description.slice(1)
 	return (
     <div className="currentDetailedWeather">
-      {description}
-      {humidity}
+      <div className="description">
+        D'oh! {descriptionCapitalized} today...
+      </div>
+      <div className="tempMax">
+        <TempMax />
+        {tempMax}<span>&deg;</span>
+      </div>
+      <div className="tempMin">
+        <TempMin />
+        {tempMin}<span>&deg;</span>
+      </div>
+      <div className="sunRise">
+        <SunRise />
+        {getDatestampInHourAndMinute(sunRise)}
+      </div>
+      <div className="sunSet">
+        <SunSet />
+        {getDatestampInHourAndMinute(sunSet)}
+      </div>
+      <div className="wind">
+        <Wind />
+        {wind} <span>km/h</span>
+      </div>
     </div>
   )
 }
